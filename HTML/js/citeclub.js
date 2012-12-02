2c860fccd7bcc48a89d775d7df3d0effc97f82bc
Peter Caisse <pcaisse@gmail.com>
2012-12-02 13:41:42 -0500

    Fixed search behavior when no (or no more) results found

--- HTML/js/citeclub.js
+++ HTML/js/citeclub.js
@@ -2,49 +2,55 @@
 var API_URL = "http://en.wikipedia.org/w/api.php";
 var CITATION_NEEDED = "citation needed";
 
-// define and instantiate class to hold page data
-function PageDataArray(){};
-PageDataArray.prototype = new Array();
-PageDataArray.prototype.unusedPages = function() {
-	return this.filter(function(page) { return page.used == false ; });
-}
-PageDataArray.prototype.hasPage = function(id) {
-	return (this.filter(function(page) { return page.id == id ; }).length > 0);
-}
-var pages = new PageDataArray(); // array to hold all citation needed page data
+// PageDataArray object to hold all relevant 'citation needed' page data
+var pages = new PageDataArray();
 
 function citationNeededPage(keywords, pageFound) {
 	
 	/*
-	* Get citation needed Wikipedia page URL
+	* Get citation needed Wikipedia page data
 	* If keywords were passed, use them to search. Otherwise, get random pages.
 	*/
 	
 	var nextUnusedPage = pages.unusedPages()[0];
 	var resultAlreadyFound = false;
+	var pageIdsReturned = new Array(); // keep track of page ids returned this iteration
 	
+	// check if there are pages stored in memory that we can use without querying Wikipedia
 	if (nextUnusedPage && keywords == nextUnusedPage.keywords) {
 		// we already have a page to use and it's related to the same keywords
 		nextUnusedPage.used = true;
 		pageFound(nextUnusedPage);
 	} else {
-		// we need to look for URLs
+		// we need to look for 'citation needed' pages
 		pageIds(keywords, function(pageIds) {
 			citationNeededPageData(keywords, pageIds, function(result) {
+			
+				// assume we won't need to retry
 				var retry = false;
+				
 				if (result != null) {
+				
+					// save page id as being returned in this iteration
+					pageIdsReturned.push(result.id);
+					
 					// if there are no unused pages, use only first one (this one)
 					result.used = (resultAlreadyFound == false && pages.unusedPages().length == 0);
+					
 					// at least one page found this time around
 					resultAlreadyFound = true;
+					
 					// add page only if hasn't already been added
 					if (!pages.hasPage(result.id)) {
+					
 						// save in array
 						pages.push(result);
+						
 						// pass result with callback only if page is new AND is to be used
 						if (result.used) {
 							pageFound(result);
 						} 
+						
 					} else {
 						// page already added, retry
 						retry = true;
@@ -54,8 +60,15 @@ function citationNeededPage(keywords, pageFound) {
 					retry = true;
 				}
 				if (retry == true) {
-					//  function calls itself again
-					citationNeededPage(keywords, pageFound);
+					// check if we should really retry
+					if (pages.hasPages(pageIdsReturned) || (pageIdsReturned.length == 0 && (keywords && keywords.length > 0))) {
+						// either we have already returned all these pages before and we're in a loop of finding the same old pages
+						// or keywords were passed and no pages were found that match them
+						pageFound(null);
+					} else {
+						// try again
+						citationNeededPage(keywords, pageFound);
+					}
 				}
 			});
 		});
@@ -88,7 +101,7 @@ function pageIds(keywords, foundPages) {
 						'format': 'json'
 					};
 	
-	// example URL: http://en.wikipedia.org/w/api.php?action=query&generator=allpages&gaplimit=500&gapfrom=dog&format=json
+	// example URL: http://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=dog&format=json
 	
 	var queryParams = isFiltered ? filteredParams : randomParams;
 	
@@ -119,8 +132,8 @@ function pageIds(keywords, foundPages) {
 function citationNeededPageData(keywords, pageIds, resultReturned) {
 
 	/*
-	* Look for [citation needed] in Wikipedia pages until one is found. 
-	* If none found, call for more random pages.
+	* Look for [citation needed] in Wikipedia pages until one is found and return its data. 
+	* If none found, return null.
 	*/
 	
 	var numFailedPages = 0;
@@ -128,37 +141,43 @@ function citationNeededPageData(keywords, pageIds, resultReturned) {
 	
 	// example: http://en.wikipedia.org/w/api.php?action=parse&pageid=736&format=json
 	
-	// iterate over page ids
-	pageIds.map(function(pageId) {
-	
-		// request Wikipedia page using page id
-		$.ajax({
-			dataType: 'jsonp',
-			url: API_URL,
-			data: {'action': 'parse', 
-				   'pageid': pageId, 
-				   'format': 'json'},
-			success: function(data) {
-				var html = data.parse.text['*'];
-				if (html.indexOf(CITATION_NEEDED) !== -1) {
-					// "citation needed" found
-					var title = data.parse.title;
-					resultReturned({'id':pageId, 'title':title, 'keywords':keywords});
-				} else {
-					// not found
-					numFailedPages++;
-					if (numFailedPages==numPages) {
-						// we've parsed all the pages we had and no dice
-						resultReturned(null);
-					}								
+	if (numPages > 0) {
+		// pages found
+		// iterate over page ids
+		pageIds.map(function(pageId) {
+		
+			// request Wikipedia page using page id
+			$.ajax({
+				dataType: 'jsonp',
+				url: API_URL,
+				data: {'action': 'parse', 
+					   'pageid': pageId, 
+					   'format': 'json'},
+				success: function(data) {
+					var html = data.parse.text['*'];
+					if (html.indexOf(CITATION_NEEDED) !== -1) {
+						// "citation needed" found
+						var title = data.parse.title;
+						resultReturned({'id':pageId, 'title':title, 'keywords':keywords});
+					} else {
+						// not found
+						numFailedPages++;
+						if (numFailedPages==numPages) {
+							// we've parsed all the pages we had and no dice
+							resultReturned(null);
+						}								
+					}
+				},
+				error: function(xhr, error){
+					console.debug(xhr);
+					console.debug(error);
 				}
-			},
-			error: function(xhr, error){
-				console.debug(xhr);
-				console.debug(error);
-			}
+			});
 		});
-	});
+	} else {
+		// no pages found
+		resultReturned(null);
+	}
 }
 
 function urlFromPageId(pageId) {
