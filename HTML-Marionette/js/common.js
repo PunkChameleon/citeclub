765d46d519b55e8ce07b08d0870a87c2bb75287b
streetlight <nweingartner@gmail.com>
2013-05-21 18:05:57 -0400

    Starting to make the Marionette App

---
+++ HTML-Marionette/js/common.js
@@ -0,0 +1,25 @@
+/*
+ *
+ * Author : streetlight
+ * Date: May 21, 2013
+ * Description: Sets up require configuration for application.
+ *
+ */
+
+define(["jquery", "backbone", "marionette"], function($, Backbone, Marionette) {
+
+    var CC = {
+        App         : {},
+        Routers     : {},
+        Controllers : {},
+        Layouts     : {},
+        Views       : {},
+        Models      : {},
+        Collections : {}
+    }
+
+    return {
+        CC : CC
+    }
+    
+});
---
+++ HTML-Marionette/js/config.js
@@ -0,0 +1,62 @@
+/*
+ *
+ * Author : streetlight
+ * Date: May 21, 2013
+ * Description: Sets up require configuration for application.
+ *
+ */
+
+requirejs.config({
+
+    baseUrl: 'js',
+
+    paths: {
+        jquery     : 'http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+        underscore : 'vendor/underscore.min',
+        backbone   : 'vendor/backbone.min',
+        marionette : 'vendor/backbone.marionette.min',
+        mustache   : 'vendor/mustache.min'
+    },
+
+    shim: {
+
+        "backbone": {
+            deps    : ["underscore", "jquery"],
+            exports : "Backbone"
+        },
+
+        "marionette" : {
+            deps    : ["underscore", "jquery", "backbone"],
+            exports : "Marionette"
+        },
+
+        "underscore" : {
+            exports : "_"
+        }
+
+    }
+
+});
+
+define(["common", "jquery", "backbone", "marionette"], function(Common, $, Backbone, Marionette) {
+
+    var CC = Common.CC;
+
+    CC.App = new Backbone.Marionette.Application();
+ 
+    // Configure custom template loading, compiling and rendering
+    CC.App.addInitializer(function(options) {
+
+        Backbone.Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
+            return Mustache.compile(rawTemplate);
+        };
+
+        Backbone.history.start();
+
+    });
+     
+    // Start the app
+    $(document).ready(function(){
+      CC.App.start();
+    });
+});
