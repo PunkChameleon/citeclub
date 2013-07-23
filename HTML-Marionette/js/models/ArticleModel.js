9e5cbf54907bc174a6d1a6f9241152c34ecb4413
streetlight <nweingartner@gmail.com>
2013-07-22 21:44:38 -0400

    Buttons views

--- HTML-Marionette/js/models/ArticleModel.js
+++ HTML-Marionette/js/models/ArticleModel.js
@@ -20,7 +20,7 @@ define([
             defaults : {
                 "articleTitle" : "Richard Stallman",
                 "articleLink" : "#",
-                "articleContent" : "'One of his criteria for giving an interview to a journalist is that the journalist agree to use his terminology throughout the article.[65] <span>Sometimes he has even required journalists to read parts of the GNU philosophy before an interview, for efficiencys sake.<sup>[citation needed]</sup></span>He has been known to turn down speaking requests over some terminology issues.[66]'"
+                "articleContent" : '"One of his criteria for giving an interview to a journalist is that the journalist agree to use his terminology throughout the article.[65] <span>Sometimes he has even required journalists to read parts of the GNU philosophy before an interview, for efficiencys sake.<sup>[citation needed]</sup></span>He has been known to turn down speaking requests over some terminology issues.[66]"'
             }
         });
 
