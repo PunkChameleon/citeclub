3af7b11025f2922bdb0b3bb0cd1286b317aea43d
Peter Caisse <pcaisse@gmail.com>
2013-08-31 15:38:02 -0400

    Real articles are now showing up

--- public/js/models/ArticleModel.js
+++ public/js/models/ArticleModel.js
@@ -16,12 +16,6 @@ define([
 
         var CC = Common.CC || {};
 
-        CC.Models.ArticleModel = Backbone.Model.extend({
-            defaults : {
-                "articleTitle" : "Richard Stallman",
-                "articleLink" : "#",
-                "articleContent" : '"One of his criteria for giving an interview to a journalist is that the journalist agree to use his terminology throughout the article.[65] <span>Sometimes he has even required journalists to read parts of the GNU philosophy before an interview, for efficiencys sake.<sup>[citation needed]</sup></span>He has been known to turn down speaking requests over some terminology issues.[66]"'
-            }
-        });
+        CC.Models.ArticleModel = Backbone.Model.extend();
 
 });
